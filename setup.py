4d1520c34e882899a56b8e11cc34fe3ea4031c22
Tom Aldcroft <taldcroft@gmail.com>
2014-08-17 18:05:50 -0400

    Initial basically-working version

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+from ska_sync.version import __version__
+
+setup(name='ska_sync',
+      author='Tom Aldcroft',
+      description='Synchronize data files for Ska runtime environment',
+      author_email='aldcroft@head.cfa.harvard.edu',
+      entry_points={'console_scripts': ['ska_sync = ska_sync.main:main']},
+      version=__version__,
+      license='BSD',
+      zip_safe=False,
+      packages=find_packages(),
+      package_data={'ska_sync': ['ska_sync.cfg']},
+      )
---
+++ ska_sync/main.py
@@ -0,0 +1,94 @@
+"""
+Synchronize data files for Ska runtime environment
+
+Examples
+========
+
+Arguments
+=========
+"""
+import os
+import re
+
+import yaml
+
+from .version import __version__
+from ska_path import ska_path
+
+PACKAGE_CONFIG_PATH = os.path.join(os.path.dirname(__file__), 'ska_sync.cfg')
+SKA_CONFIG_PATH = os.path.join(ska_path(), 'ska_sync.cfg')  # `None` if path doesn't exist
+
+def get_opt():
+    import argparse
+    parser = argparse.ArgumentParser(description=__doc__,
+                                     formatter_class=argparse.RawDescriptionHelpFormatter)
+
+    parser.add_argument('--user',
+                        help='User name for remote host')
+
+    parser.add_argument('--install',
+                        action='store_true',
+                        help='Install sync config file in default location')
+
+    parser.add_argument('--force',
+                        action='store_true',
+                        help='Force overwrite of sync config file')
+
+    opt = parser.parse_args()
+    return opt
+
+
+def install(opt):
+    import shutil
+    in_path = PACKAGE_CONFIG_PATH
+    out_path = SKA_CONFIG_PATH
+    if os.path.exists(out_path) and not opt.force:
+        print('ERROR: ska sync config file {} already exists.  Use --force option to overwrite.')
+
+    with open(in_path, 'r') as fh:
+        config_text = fh.read()
+
+    # Change hardwired default comment
+    if opt.user:
+        config_text = re.sub(r'# user: name', 'user: {}'.format(opt.user), config_text)
+
+    with open(out_path, 'w') as fh:
+        fh.write(config_text)
+
+    print('Wrote ska sync config file to {}'.format(out_path))
+
+
+def file_sync(packages, user, host):
+    """
+    Sync files for packages assuming location in $SKA/data/<package>/.
+    """
+    files = []
+    for package in sorted(packages):
+        paths = packages[package]
+        for path in sorted(paths):
+            files.append('data/{}/{}\n'.format(package, path))
+
+    with open('rsync_files.tmp', 'w') as fh:
+        fh.writelines(files)
+
+    rsync_cmd = ()
+    print('\n'
+          'rsync -arzv --size-only --files-from=rsync_files.tmp \\\n'
+          '   {user}@{host}:/proj/sot/ska/ {ska_path}/'
+          .format(user=user, host=host, ska_path=ska_path()))
+
+
+def main():
+    opt = get_opt()
+    if opt.install:
+        install(opt)
+        return
+
+    config_path = SKA_CONFIG_PATH or PACKAGE_CONFIG_PATH
+    config = yaml.safe_load(open(config_path, 'r'))
+    print('Loaded config from {}'.format(config_path))
+
+    # Remote user name
+    user = opt.user or config.get('user') or os.environ['USER']
+
+    file_sync(config['file_sync'], user, config['host'])
