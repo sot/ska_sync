49b08e7a6ce9a8c65dc68edffbfd288e99ff4754
Javier Gonzalez <javierggt@yahoo.com>
2019-12-17 12:57:36 -0500

    Added changes to use setuptools_scm

--- setup.py
+++ setup.py
@@ -1,13 +1,12 @@
 from setuptools import setup, find_packages
 
-from ska_sync.version import __version__
-
 setup(name='ska_sync',
       author='Tom Aldcroft',
       description='Synchronize data files for Ska3 runtime environment',
       author_email='aldcroft@head.cfa.harvard.edu',
       entry_points={'console_scripts': ['ska_sync = ska_sync.main:main']},
-      version=__version__,
+      use_scm_version=True,
+      setup_requires=['setuptools_scm', 'setuptools_scm_git_archive'],
       license='BSD',
       zip_safe=False,
       packages=find_packages(),
